83d7524bc3ae42ea02ccf41b1da287784d42717e
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2018-09-30 17:41:45 +0330

    Adding test unit test to the project

---
+++ src/test/java/com/gazmeh/jmeter/JUnit5ExampleTest.java
@@ -0,0 +1,11 @@
+package com.gazmeh.jmeter;
+
+import org.junit.Test;
+
+class JUnit5ExampleTest {
+ 
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+    }
+}
